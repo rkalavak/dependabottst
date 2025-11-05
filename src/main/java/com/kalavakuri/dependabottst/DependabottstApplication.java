52d7def31ccd55bd7b2e5129b690ecc2863d9ec1
rkalavak <kalavakurirenu@gmail.com>
2025-11-05 08:50:38 +0530

    Initial commit

---
+++ src/main/java/com/kalavakuri/dependabottst/DependabottstApplication.java
@@ -0,0 +1,13 @@
+package com.kalavakuri.dependabottst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependabottstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependabottstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalavakuri/dependabottst/DependabottstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalavakuri.dependabottst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependabottstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
